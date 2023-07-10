dce5339322bf48adaeabb4c3d372c77bbcec148c
Homrai <homraiml@hotmail.com>
2023-07-09 19:30:29 -0500

    loading menu tamaño letras carga

--- src/app/menu/loading.jsx
+++ src/app/menu/loading.jsx
@@ -2,7 +2,7 @@
 export default function Loading() {
  
  return (
- <div className="text-shadow-lg shadow-black text-9xl items-center flex">
+ <div className="text-shadow-lg shadow-black md:text-9xl text-3xl items-center flex">
     <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
 
     </svg>
